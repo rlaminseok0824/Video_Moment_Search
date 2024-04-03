1a576a68161627ebfe5fed31341e832192634458
rlaminseok0824 <xornjsrlaals@gmail.com>
2024-04-03 20:45:08 +0900

    ADD: add app initial page

---
+++ lib/app_home_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppHomePage extends StatefulWidget {
+  const AppHomePage({super.key});
+
+  @override
+  State<AppHomePage> createState() => _AppHomePageState();
+}
+
+class _AppHomePageState extends State<AppHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
